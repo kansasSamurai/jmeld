af53722eefb06bac3e0e56aa7dc7fb37188ebcd1
kansasSamurai <kansasSamurai@users.noreply.github.com>
2018-11-19 12:20:43 -0600

    Update copyright and license notice

--- src/main/java/org/jmeld/ui/diffbar/DiffScrollComponent.java
+++ src/main/java/org/jmeld/ui/diffbar/DiffScrollComponent.java
@@ -1,20 +1,35 @@
-package org.jmeld.ui.diffbar;
 /*
    JMeld is a visual diff and merge tool.
-   Copyright (C) 2007  Kees Kuip
+   
+   -----
+   Copyright (C) 2018  Rick Wellman
+   
+   This library is free software and has been modified according to the permissions 
+   granted below; this version of the library continues to be distributed under the terms of the
+   GNU Lesser General Public License version 2.1 as published by the Free Software Foundation
+   and may, therefore, be redistributed or further modified under the same terms as the original.
+   
+   -----
+   Copyright (C) 2007  Kees Kuip - GNU LGPL
+   
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2.1 of the License, or (at your option) any later version.
+   
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+   
+   See the GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General 
+   Public License along with this library; if not, write to:
+   Free Software Foundation, Inc.
+   51 Franklin Street, Fifth Floor
    Boston, MA  02110-1301  USA
  */
+package org.jmeld.ui.diffbar;
 
 import java.awt.Color;
 import java.awt.Graphics;
@@ -58,7 +73,8 @@ import org.jmeld.util.conf.ConfigurationListenerIF;
 
 /**
  * 
- * @author jmeld-legacy
+ * @author jmeld-legacy committed
+ * @author Rick Wellman
  *
  */
 @SuppressWarnings("serial")

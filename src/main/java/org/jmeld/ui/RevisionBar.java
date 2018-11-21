a5a09e28ac232dc7f82ae12032c6d08ab2ba0f85
kansasSamurai <kansasSamurai@users.noreply.github.com>
2018-11-21 13:15:33 -0600

    Apply standard formatting and style + Update copyright and license notice

--- src/main/java/org/jmeld/ui/RevisionBar.java
+++ src/main/java/org/jmeld/ui/RevisionBar.java
@@ -1,210 +1,204 @@
-/*
+/*   
+   JWeld - A diff and merge API plus GUI - Originally forked from JMeld
+   Copyright (C) 2018  Rick Wellman - GNU LGPL
+   
+   This library is free software and has been modified according to the permissions 
+   granted below; this version of the library continues to be distributed under the terms of the
+   GNU Lesser General Public License version 2.1 as published by the Free Software Foundation
+   and may, therefore, be redistributed or further modified under the same terms as the original.
+   
+   -----
    JMeld is a visual diff and merge tool.
-   Copyright (C) 2007  Kees Kuip
+   Copyright (C) 2007  Kees Kuip - GNU LGPL
+   
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2.1 of the License, or (at your option) any later version.
+   
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+   
+   See the GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General 
+   Public License along with this library; if not, write to:
+   Free Software Foundation, Inc.
+   51 Franklin Street, Fifth Floor
    Boston, MA  02110-1301  USA
+   
  */
 package org.jmeld.ui;
 
-import org.jmeld.diff.JMChunk;
-import org.jmeld.diff.JMDelta;
-import org.jmeld.diff.JMRevision;
-import org.jmeld.ui.text.BufferDocumentIF;
-import org.jmeld.ui.util.ColorUtil;
-import org.jmeld.ui.util.Colors;
-import org.jmeld.ui.util.RevisionUtil;
-
-import javax.swing.*;
-import javax.swing.text.JTextComponent;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class RevisionBar
-    extends JComponent
-{
-  private BufferDiffPanel diffPanel;
-  private FilePanel filePanel;
-  private boolean original;
+import javax.swing.AbstractButton;
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JScrollBar;
 
-  public RevisionBar(BufferDiffPanel diffPanel, FilePanel filePanel,
-      boolean original)
-  {
-    this.diffPanel = diffPanel;
-    this.filePanel = filePanel;
-    this.original = original;
+import org.jmeld.diff.JMChunk;
+import org.jmeld.diff.JMDelta;
+import org.jmeld.diff.JMRevision;
+import org.jmeld.ui.util.ColorUtil;
+import org.jmeld.ui.util.Colors;
+import org.jmeld.ui.util.RevisionUtil;
 
-    setBorder(BorderFactory.createLineBorder(ColorUtil.darker(ColorUtil
-        .darker(Colors.getPanelBackground()))));
+/**
+ * This custom JComponent is the visual "diff summary" displayed on the side of each BufferDiffPanel.
+ * 
+ * @author jmeld-legacy
+ * @author Rick Wellman
+ *
+ */
+@SuppressWarnings("serial")
+public class RevisionBar extends JComponent {
+    
+    private FilePanel filePanel;
 
-    addMouseListener(getMouseListener());
-  }
+    private BufferDiffPanel diffPanel;
+  
+    /** If true, this represents the "original/left" document. Otherwise, it represents the "revised/right" document. */
+    private boolean original;
 
-  private MouseListener getMouseListener()
-  {
-    return new MouseAdapter()
-    {
-      public void mouseClicked(MouseEvent me)
-      {
-        Rectangle r;
-        int y;
-        int line;
-        int lineBefore;
-        int lineAfter;
-        JMRevision revision;
-        Point p;
-        BufferDocumentIF bd;
-        int offset;
-        JTextComponent editor;
-        JViewport viewport;
-        int numberOfLines;
-        JMChunk original;
+    public RevisionBar(BufferDiffPanel diffPanel, FilePanel filePanel, boolean original) {
 
-        r = getDrawableRectangle();
-        if (r == null)
-        {
-          return;
+        this.diffPanel = diffPanel;
+        this.filePanel = filePanel;
+        this.original = original;
+
+        setBorder(BorderFactory.createLineBorder(
+            ColorUtil.darker(
+            ColorUtil.darker(
+                    Colors.getPanelBackground()))   
+                ));
+
+        addMouseListener(getMouseListener());
+    }
+
+    private MouseListener getMouseListener() {
+        return new MouseAdapter() {
+            public void mouseClicked(MouseEvent me) {
+
+                final Rectangle r = getDrawableRectangle();
+                if (r == null) {
+                    return;
+                } else if (r.height <= 0) {
+                    return;
+                }
+
+                final JMRevision revision = diffPanel.getCurrentRevision();
+                if (revision == null) {
+                    return;
+                }
+
+                final int y = me.getY() - r.y;
+                final int numberOfLines = getNumberOfLines(revision);
+
+                int line = (y * numberOfLines) / r.height;
+                if (line > numberOfLines) {
+                    line = numberOfLines;
+                }
+                if (line < 0) {
+                    line = 0;
+                }
+
+                // If the files are very large the resolution of one pixel contains a lot of
+                // lines of the document.
+                // Check if there is a chunk in the revision between those lines and if there is
+                // position on that chunk.
+                final int lineBefore = ((y - 3) * numberOfLines) / r.height;
+                final int lineAfter = ((y + 3) * numberOfLines) / r.height;
+                for (JMDelta delta : revision.getDeltas()) {
+                    final JMChunk original = delta.getOriginal();
+
+                    // The chunk starts within the bounds of the line-resolution.
+                    if (original.getAnchor() > lineBefore && original.getAnchor() < lineAfter) {
+                        diffPanel.doGotoDelta(delta);
+                        return;
+                    }
+                }
+
+                diffPanel.doGotoLine(line);
+            } // end mouseclicked
+        };
+    } // end mouselistener
+
+    /**
+     * Calculate the rectangle that can be used to draw the diffs. It is essentially
+     * the size of the scrollbar minus its buttons.
+     */
+    private Rectangle getDrawableRectangle() {
+
+        final JScrollBar sb = filePanel.getScrollPane().getVerticalScrollBar();
+        final Rectangle r = sb.getBounds();
+        r.x = 0;
+        r.y = 0;
+
+        for (Component c : sb.getComponents()) {
+            if (c instanceof AbstractButton) {
+                r.y += c.getHeight();
+                r.height -= (2 * c.getHeight());
+                break;
+            }
         }
 
-        if (r.height <= 0)
-        {
-          return;
-        }
+        return r;
+    }
 
-        y = me.getY() - r.y;
+    public void paintComponent(Graphics g) {
+        final Graphics2D g2 = (Graphics2D) g;
+        
+        final Rectangle clipBounds = g.getClipBounds();
+        final Rectangle r = getDrawableRectangle();
+        r.x = clipBounds.x;
+        r.width = clipBounds.width;
 
-        revision = diffPanel.getCurrentRevision();
-        if (revision == null)
-        {
-          return;
-        }
+        // Paint the background (in white)
+        g2.setColor(Color.white);
+        g2.fill(r);
 
-        numberOfLines = getNumberOfLines(revision);
-        line = (y * numberOfLines) / r.height;
-        if (line > numberOfLines)
-        {
-          line = numberOfLines;
-        }
-
-        if (line < 0)
-        {
-          line = 0;
-        }
-
-        // If the files are very large the resolution of one pixel contains 
-        //   a lot of lines of the document. Check if there is a chunk in 
-        //   the revision between those lines and if there is position on 
-        //   that chunk.
-        lineBefore = ((y - 3) * numberOfLines) / r.height;
-        lineAfter = ((y + 3) * numberOfLines) / r.height;
-        for (JMDelta delta : revision.getDeltas())
-        {
-          original = delta.getOriginal();
-
-          // The chunk starts within the bounds of the line-resolution.
-          if (original.getAnchor() > lineBefore
-              && original.getAnchor() < lineAfter)
-          {
-            diffPanel.doGotoDelta(delta);
+        // If there are no revisions, no need to paint anything else
+        final JMRevision revision = diffPanel.getCurrentRevision();
+        if (revision == null) {
             return;
-          }
         }
 
-        diffPanel.doGotoLine(line);
-      }
-    };
-  }
+        // If there are "no lines", no need to paint anything else
+        final int numberOfLines = getNumberOfLines(revision);
+        if (numberOfLines <= 0) {
+            return;
+        }
 
-  /** Calculate the rectangle that can be used to draw the diffs.
-   *    It is essentially the size of the scrollbar minus its buttons.
-   */
-  private Rectangle getDrawableRectangle()
-  {
-    JScrollBar sb;
-    Rectangle r;
-    int buttonHeight;
+        // Finally, paint each delta
+        for (JMDelta delta : revision.getDeltas()) {
 
-    sb = filePanel.getScrollPane().getVerticalScrollBar();
-    r = sb.getBounds();
-    r.x = 0;
-    r.y = 0;
+            final JMChunk chunk = original ? delta.getOriginal() : delta.getRevised();
 
-    for (Component c : sb.getComponents())
-    {
-      if (c instanceof AbstractButton)
-      {
-        r.y += c.getHeight();
-        r.height -= (2 * c.getHeight());
-        break;
-      }
+            // Calculate geometry
+            final int y = r.y + (r.height * chunk.getAnchor()) / numberOfLines;
+            final int heightCalc = (r.height * chunk.getSize()) / numberOfLines;
+            final int height = heightCalc <= 0 ? 1 : heightCalc;
+            
+            // Set the color corresponding to the delta type then paint the delta
+            g2.setColor(RevisionUtil.getColor(delta));
+            g2.fillRect(0, y, r.width, height);
+            
+        }
+        
+        g2.dispose();
     }
 
-    return r;
-  }
-
-  public void paintComponent(Graphics g)
-  {
-    Rectangle r;
-    Graphics2D g2;
-    JMRevision revision;
-    JMChunk chunk;
-    int y;
-    int height;
-    int numberOfLines;
-    Rectangle clipBounds;
-
-    g2 = (Graphics2D) g;
-
-    clipBounds = g.getClipBounds();
-
-    r = getDrawableRectangle();
-    r.x = clipBounds.x;
-    r.width = clipBounds.width;
-
-    g2.setColor(Color.white);
-    g2.fill(r);
-
-    revision = diffPanel.getCurrentRevision();
-    if (revision == null)
-    {
-      return;
+    private int getNumberOfLines(JMRevision revision) {
+        return original ? revision.getOrgSize() : revision.getRevSize();
     }
 
-    numberOfLines = getNumberOfLines(revision);
-    if (numberOfLines <= 0)
-    {
-      return;
-    }
-
-    for (JMDelta delta : revision.getDeltas())
-    {
-      chunk = original ? delta.getOriginal() : delta.getRevised();
-
-      g.setColor(RevisionUtil.getColor(delta));
-      y = r.y + (r.height * chunk.getAnchor()) / numberOfLines;
-      height = (r.height * chunk.getSize()) / numberOfLines;
-      if (height <= 0)
-      {
-        height = 1;
-      }
-
-      g.fillRect(0, y, r.width, height);
-    }
-  }
-
-  private int getNumberOfLines(JMRevision revision)
-  {
-    return original ? revision.getOrgSize() : revision.getRevSize();
-  }
 }

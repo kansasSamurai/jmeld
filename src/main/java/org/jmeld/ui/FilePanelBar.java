af79369ca810b370a660097c3bb8448ea40d6059
kansasSamurai <kansasSamurai@users.noreply.github.com>
2018-11-21 12:34:18 -0600

    Update copyright and license notice

--- src/main/java/org/jmeld/ui/FilePanelBar.java
+++ src/main/java/org/jmeld/ui/FilePanelBar.java
@@ -1,18 +1,34 @@
 /*
+   
+   JWeld - A diff and merge API plus GUI - Originally forked from JMeld
+   Copyright (C) 2018  Rick Wellman - GNU LGPL
+   
+   This library is free software and has been modified according to the permissions 
+   granted below; this version of the library continues to be distributed under the terms of the
+   GNU Lesser General Public License version 2.1 as published by the Free Software Foundation
+   and may, therefore, be redistributed or further modified under the same terms as the original.
+   
+   -----
    JMeld is a visual diff and merge tool.
-   Copyright (C) 2007  Kees Kuip
+   Copyright (C) 2007  Kees Kuip - GNU LGPL
+   
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2.1 of the License, or (at your option) any later version.
+   
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+   
+   See the GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General 
+   Public License along with this library; if not, write to:
+   Free Software Foundation, Inc.
+   51 Franklin Street, Fifth Floor
    Boston, MA  02110-1301  USA
+   
  */
 package org.jmeld.ui;
 
@@ -21,32 +37,42 @@ import org.jmeld.ui.util.ToolBarBuilder;
 
 import javax.swing.*;
 
-public class FilePanelBar
-    extends JPanel
-{
-  private FilePanel filePanel;
-  private JLabel selected;
-  private JLabel lineNumber;
-  private JLabel columnNumber;
-  private ImageIcon iconSelected;
-  private ImageIcon iconNotSelected;
+/**
+ * A type of "status bar" for the main FilePanel (a.k.a. FileViewer)
+ * <p>
+ * Includes such information as:<ul>
+ * <li>Line x of y</li>
+ * <li>Column</li>
+ * <li>Visual indicator that current file panel has the focus</li>
+ * 
+ * @author jmeld-legacy
+ * @author Rick Wellman
+ *
+ */
+@SuppressWarnings("serial")
+public class FilePanelBar extends JPanel {
+    
+    /** A reference to the FilePanel being statused/decorated */
+    private FilePanel filePanel;
+    private JLabel selected;
+    private JLabel lineNumber;
+    private JLabel columnNumber;
+    private ImageIcon iconSelected;
+    private ImageIcon iconNotSelected;
 
-  public FilePanelBar(FilePanel filePanel)
-  {
+  public FilePanelBar(FilePanel filePanel) {
     this.filePanel = filePanel;
 
     init();
   }
 
-  private void init()
-  {
-    ToolBarBuilder builder;
-
+  private void init() {
+      
     selected = new JLabel();
     lineNumber = new JLabel();
     columnNumber = new JLabel();
 
-    builder = new ToolBarBuilder(this);
+    final ToolBarBuilder builder = new ToolBarBuilder(this);
     builder.addComponent(selected);
     builder.addSpring();
     builder.addComponent(lineNumber);
@@ -59,8 +85,8 @@ public class FilePanelBar
     update();
   }
 
-  public void update()
-  {
+  public void update() {
+      
     Icon icon;
     JTextArea editor;
     int caretPosition;
@@ -76,21 +102,15 @@ public class FilePanelBar
 
     editor = filePanel.getEditor();
     caretPosition = editor.getCaretPosition();
-    try
-    {
+    try {
       line = editor.getLineOfOffset(caretPosition);
-    }
-    catch (Exception ex)
-    {
+    } catch (Exception ex) {
       line = -1;
     }
 
-    try
-    {
+    try {
       column = caretPosition - editor.getLineStartOffset(line);
-    }
-    catch (Exception ex)
-    {
+    } catch (Exception ex) {
       column = -1;
     }
 
@@ -100,4 +120,5 @@ public class FilePanelBar
     text = String.format("Column: %03d", column);
     columnNumber.setText(text);
   }
+  
 }

5025d7d66680057196166ddf9a7c0e18c1b30a92
kansasSamurai <kansasSamurai@users.noreply.github.com>
2018-11-30 10:18:54 -0600

    Added alpha channel to color settings

--- src/main/java/org/jmeld/settings/ColorSetting.java
+++ src/main/java/org/jmeld/settings/ColorSetting.java
@@ -1,65 +1,89 @@
-/*
+/* 
+   JWeld - A diff and merge API plus GUI - Originally forked from JMeld
+   Copyright (C) 2018  Rick Wellman - GNU LGPL
+   
+   This library is free software and has been modified according to the permissions 
+   granted below; this version of the library continues to be distributed under the terms of the
+   GNU Lesser General Public License version 2.1 as published by the Free Software Foundation
+   and may, therefore, be redistributed or further modified under the same terms as the original.
+   
+   -----
    JMeld is a visual diff and merge tool.
-   Copyright (C) 2007  Kees Kuip
+   Copyright (C) 2007  Kees Kuip - GNU LGPL
+   
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2.1 of the License, or (at your option) any later version.
+   
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+   
+   See the GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General 
+   Public License along with this library; if not, write to:
+   Free Software Foundation, Inc.
+   51 Franklin Street, Fifth Floor
    Boston, MA  02110-1301  USA
+   
  */
 package org.jmeld.settings;
 
-import org.jmeld.ui.util.*;
-import org.jmeld.util.conf.*;
+import java.awt.Color;
 
-import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 
-import java.awt.*;
+import org.jmeld.util.conf.AbstractConfigurationElement;
 
+/**
+ * 
+ * @author jmeld-legacy
+ * @author Rick Wellman
+ *
+ */
 @XmlAccessorType(XmlAccessType.NONE)
-public class ColorSetting
-    extends AbstractConfigurationElement
-{
-  @XmlAttribute
-  private int b = -1;
-  @XmlAttribute
-  private int g = -1;
-  @XmlAttribute
-  private int r = -1;
-  private Color color;
+public class ColorSetting extends AbstractConfigurationElement {
+    
+    @XmlAttribute
+    private int a = -1;
 
-  public ColorSetting()
-  {
-  }
+    @XmlAttribute
+    private int b = -1;
 
-  public ColorSetting(Color color)
-  {
-    this.r = color.getRed();
-    this.g = color.getGreen();
-    this.b = color.getBlue();
+    @XmlAttribute
+    private int g = -1;
 
-    this.color = color;
-  }
+    @XmlAttribute
+    private int r = -1;
 
-  public Color getColor()
-  {
-    if (r == -1 || g == -1 || b == -1)
-    {
-      return null;
+    private Color color;
+
+    public ColorSetting() {
     }
 
-    if (color == null)
-    {
-      color = new Color(r, g, b);
+    public ColorSetting(Color color) {
+        this.r = color.getRed();
+        this.g = color.getGreen();
+        this.b = color.getBlue();
+        this.a = color.getAlpha();
+
+        this.color = color;
     }
 
-    return color;
-  }
+    public Color getColor() {
+        if (r == -1 || g == -1 || b == -1 || a == -1) {
+            return null;
+        }
+
+        if (color == null) {
+            color = new Color(r, g, b, a);
+        }
+
+        return color;
+    }
+
 }

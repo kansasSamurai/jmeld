3ccff4929ef5248112a89665de2d1f1ef90fb079
kansasSamurai <kansasSamurai@users.noreply.github.com>
2018-12-05 13:33:41 -0600

    An example implementation of CompareAware

---
+++ src/main/java/org/jmeld/diff/CompareCRISP.java
@@ -0,0 +1,16 @@
+package org.jmeld.diff;
+
+/**
+ * 
+ * @author Rick Wellman
+ *
+ */
+public class CompareCRISP extends BaseCompare implements CompareAware {
+
+    @Override
+    public boolean compare(Object o1, Object o2) {
+
+        return false;
+    }
+
+}

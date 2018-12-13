6de8e7115766764692769573c5becc39c21f98c4
kansasSamurai <kansasSamurai@users.noreply.github.com>
2018-12-13 17:20:41 -0600

    Update copyright

--- src/main/java/org/jmeld/JMeld.java
+++ src/main/java/org/jmeld/JMeld.java
@@ -1,18 +1,33 @@
-/*
+/* 
+   JWeld - A diff and merge API plus GUI - Originally forked from JMeld
+   Copyright (C) 2018  Rick Wellman - GNU LGPL
+   
+   This library is free software and has been modified according to the permissions 
+   granted below; this version of the library continues to be distributed under the terms of the
+   GNU Lesser General Public License version 2.1 as published by the Free Software Foundation
+   and may, therefore, be redistributed or further modified under the same terms as the original.
+   
+   -----
    JMeld is a visual diff and merge tool.
-   Copyright (C) 2007  Kees Kuip
+   Copyright (C) 2007  Kees Kuip - GNU LGPL
+   
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2.1 of the License, or (at your option) any later version.
+   
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+   
+   See the GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General 
+   Public License along with this library; if not, write to:
+   Free Software Foundation, Inc.
+   51 Franklin Street, Fifth Floor
    Boston, MA  02110-1301  USA
+   
  */
 package org.jmeld;
 
@@ -34,6 +49,7 @@ import org.jmeld.util.prefs.WindowPreference;
 /**
  * 
  * @author jmeld-legacy
+ * @author Rick Wellman
  *
  */
 public class JMeld implements Runnable {
@@ -78,19 +94,18 @@ public class JMeld implements Runnable {
     }
 
     public static void main(String[] args) {
-        final JMeldSettings settings = JMeldSettings.getInstance();
+        
+        final JMeldSettings settings = JMeldSettings.getInstance();        
         settings.getEditor().setShowLineNumbers(true);
-        
-        // TODO Bug: if setDrawCurves is true, F7/F8 do not work correctly
-        // Actually, it seems to manifest itself even when false...
-        // the JSplitPane appears to get focus when hitting F7/F8 instead of the DiffScrollComponent
-        settings.setDrawCurves(false); // original setting is:  true
-        settings.setCurveType(1);
-        
         if (settings.getEditor().isAntialiasEnabled()) {
             System.setProperty("swing.aatext", "true");
         }
-
+        // TODO Bug: if setDrawCurves is true, F7/F8 do not work correctly
+        // Actually, it seems to manifest itself even when false...
+        // the JSplitPane appears to get focus when hitting F7/F8 instead of the DiffScrollComponent
+        // settings.setDrawCurves(false); // original setting is:  true
+        // settings.setCurveType(1);
+        
         IconFontSwing.register(FontAwesome.getIconFont());
 
         SwingUtilities.invokeLater(new JMeld(args));
